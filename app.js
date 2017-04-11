2c8929af9197fef1980069ab9ed99ccebbff26d9
NiteshRaghav <raghuvanshirgv@gmail.com>
2017-04-11 16:19:33 +0530

    second

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ var exphbs = require('express-handlebars');
 var expressValidator = require('express-validator');
 var flash = require('connect-flash');
 var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+
 //////////////////////////////////////////
 mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
